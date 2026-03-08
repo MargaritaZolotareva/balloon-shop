10ff93e2030791aa09a5a0980a56f7dd3540a435
Margarita <mrg.zolotareva@gmail.com>
2026-03-08 15:02:15 +0500

    feat: #22 разделить chunk-vendors на js-ники поменьше, сделать монтирование app после загрузки router

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -13,6 +13,27 @@ module.exports = defineConfig(
         chainWebpack: config => {
             config.resolve.alias.set('vue$', 'vue/dist/vue.esm-bundler.js');
         },
+        configureWebpack: {
+            optimization: {
+                splitChunks: {
+                    chunks: 'all',
+                    cacheGroups: {
+                        vue: {
+                            test: /[\\/]node_modules[\\/](vue|vue-router)[\\/]/,
+                            name: 'vue-core',
+                            chunks: 'all',
+                            priority: 20
+                        },
+                        vendors: {
+                            test: /[\\/]node_modules[\\/]/,
+                            name: 'vendors',
+                            chunks: 'all',
+                            priority: 10
+                        }
+                    }
+                }
+            }
+        },
         pages: {
             index: {
                 entry: 'src/main.js',
